50666413513814187b6f98dab04d0189a34c2731
tvpis <tvpis@tvpis.org>
2017-07-25 13:47:36 +0200

    More propaganda; update gen.rb for new directory structure

--- gen.rb
+++ gen.rb
@@ -1,3 +1,3 @@
 #!ruby
 
-puts "loadImages(['" + Dir["*.jpg"].to_a.sort_by{|n| n.split(/ /).last.to_i }.join("', '") + "']);"
+puts "loadImages(['" + Dir["img/*.jpg"].to_a.map{|n| File.basename(n) }.sort_by{|n| n.split(/ /).last.to_i }.join("', '") + "']);"
