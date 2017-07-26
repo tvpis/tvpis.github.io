a65338d96b97a6a66e4e1205f1bd58f44162b15d
tvpis <tvpis@tvpis.org>
2017-07-26 18:00:42 +0200

    Make it easier to add more propaganda

--- gen.rb
+++ gen.rb
@@ -1,3 +1,12 @@
 #!ruby
+require "fileutils"
+files = Dir["img/*.jpg"].to_a.map{|n| File.basename(n) }.sort_by{|n| n.split(/ /).last.to_i }
+last = files.last.to_i
+Dir["raw/src/*"].each do |raw|
+    last += 1
+    system("convert -resize 1920x1080 '%s' 'img/%d.jpg'" % [raw, last])
+    FileUtils.mv(raw, "raw/done/")
+    files << "#{last}.jpg"
+end
 
-puts "loadImages(['" + Dir["img/*.jpg"].to_a.map{|n| File.basename(n) }.sort_by{|n| n.split(/ /).last.to_i }.join("', '") + "']);"
+puts "loadImages(['" + files.join("', '") + "']);"
